7f70be05efb04d84df126a07d1695c047cfc06d5
Harini Sivakumar <63298200+Harry12901@users.noreply.github.com>
2020-12-01 17:25:17 +0530

    Create Logs.java

---
+++ src/Logs.java
@@ -0,0 +1,17 @@
+class Logs
+{
+	Scanner input = new Scanner(System.in);
+	public int LogID;
+	public int RoadID,PoliceID,StationID;
+	public String type;
+	public LocalDate date = LocalDate.now();
+	public LocalTime time = LocalTime.now();
+	public Logs(int logid,int roadid,int policeid,int SID,String type ) 
+	{
+		LogID=logid;
+		RoadID=roadid;
+		PoliceID=policeid;
+		StationID=SID;
+		this.type=type;
+	 }
+}
